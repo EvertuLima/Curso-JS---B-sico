0d46276ed4f7d840dd9519378f6be0494f0d0d21
EvertuLima <78697248+EvertuLima@users.noreply.github.com>
2023-09-25 15:56:45 -0300

    o que não se pode fazer com arrow function

--- 7_arrow_function/js/scripts.js
+++ 7_arrow_function/js/scripts.js
@@ -7,7 +7,7 @@
 let a = 5;
 let b = 10;
 
-let c = (num1, num2) => a + b;
+let c = (num1, num2) => num1 + num2;
 
 console.log(c(a,b))
 
@@ -16,3 +16,39 @@ let list = ['a', 'b', 'c', 'd'];
 let list2 = list.filter(list => list !== 'a');
 
 console.log(list2);
+
+let animals = [
+    { name: 'Turca', type: 'Dog', age: 12 },
+    { name: 'Tom', type: 'Cat', age: 3 },
+    { name: 'Frida', type: 'Dog', age: 16},
+    { name: 'Bob', type: 'Horse', age: 13},
+];
+
+let animalsVelhos = animals.filter((animal) => animal.age > 10)
+
+console.log(animalsVelhos)
+
+// NÃO USAR ARROW
+
+let pessoa = {
+    nome : "Everton",
+
+    sayName: () => {
+       console.log(this.nome)
+    }
+}
+pessoa.sayName();
+
+// let btn = document.getElementById('btn');
+
+// btn.addEventListener('click',  function(){
+//    this.classList.toggle('classe');
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.getElementById('btn');
+  
+    btn.addEventListener('click', function() {
+      this.classList.toggle('classe');
+    });
+  });
